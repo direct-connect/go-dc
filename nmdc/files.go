5cff092ded8b4c2b9104942f16664a14ba5ce014
gswly <46489434+gswly@users.noreply.github.com>
2019-08-15 19:20:29 +0200

    nmdc: add ADCGET, ADCSND, Direction, MaxedOut

---
+++ nmdc/files.go
@@ -0,0 +1,237 @@
+package nmdc
+
+import (
+	"bytes"
+	"errors"
+	"strconv"
+)
+
+func init() {
+	RegisterMessage(&ADCGet{})
+	RegisterMessage(&ADCSnd{})
+	RegisterMessage(&Direction{})
+	RegisterMessage(&MaxedOut{})
+}
+
+type ADCGet struct {
+	ContentType String
+	Identifier  String
+	Start       uint64
+	Length      int64
+	Compressed  bool
+}
+
+func (*ADCGet) Type() string {
+	return "ADCGET"
+}
+
+func (m *ADCGet) MarshalNMDC(enc *TextEncoder, buf *bytes.Buffer) error {
+	if err := m.ContentType.MarshalNMDC(enc, buf); err != nil {
+		return err
+	}
+	buf.WriteByte(' ')
+	if err := m.Identifier.MarshalNMDC(enc, buf); err != nil {
+		return err
+	}
+	buf.WriteByte(' ')
+	buf.WriteString(strconv.FormatUint(m.Start, 10))
+	buf.WriteByte(' ')
+	buf.WriteString(strconv.FormatInt(m.Length, 10))
+	if m.Compressed {
+		buf.Write([]byte(" ZL1"))
+	}
+	return nil
+}
+
+func (m *ADCGet) UnmarshalNMDC(dec *TextDecoder, data []byte) error {
+	i := bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCGet: missing separator after field 1")
+	}
+	contentType, data := data[:i], data[i+1:]
+	if err := m.ContentType.UnmarshalNMDC(dec, contentType); err != nil {
+		return err
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCGet: missing separator after field 2")
+	}
+	identifier, data := data[:i], data[i+1:]
+	if err := m.Identifier.UnmarshalNMDC(dec, identifier); err != nil {
+		return errors.New("ADCGet: unable to parse field 2")
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCGet: missing separator after field 3")
+	}
+	start, data := data[:i], data[i+1:]
+	var err error
+	m.Start, err = parseUin64Trim(start)
+	if err != nil {
+		return errors.New("ADCGet: unable to parse field 3")
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	var length []byte
+	if i < 0 {
+		length, data = data[:], nil
+	} else {
+		length, data = data[:i], data[i+1:]
+	}
+	if bytes.Equal(length, []byte("-1")) {
+		m.Length = -1
+	} else {
+		lengthNum, err := parseUin64Trim(length)
+		if err != nil {
+			return errors.New("ADCGet: unable to parse field 4")
+		}
+		m.Length = int64(lengthNum)
+	}
+
+	if len(data) > 0 {
+		if !bytes.Equal(data, []byte("ZL1")) {
+			return errors.New("ADCGet: invalid field 5")
+		}
+		m.Compressed = true
+	}
+
+	return nil
+}
+
+type ADCSnd struct {
+	ContentType String
+	Identifier  String
+	Start       uint64
+	Length      uint64
+	Compressed  bool
+}
+
+func (*ADCSnd) Type() string {
+	return "ADCSND"
+}
+
+func (m *ADCSnd) MarshalNMDC(enc *TextEncoder, buf *bytes.Buffer) error {
+	if err := m.ContentType.MarshalNMDC(enc, buf); err != nil {
+		return err
+	}
+	buf.WriteByte(' ')
+	if err := m.Identifier.MarshalNMDC(enc, buf); err != nil {
+		return err
+	}
+	buf.WriteByte(' ')
+	buf.WriteString(strconv.FormatUint(m.Start, 10))
+	buf.WriteByte(' ')
+	buf.WriteString(strconv.FormatUint(m.Length, 10))
+	if m.Compressed {
+		buf.Write([]byte(" ZL1"))
+	}
+	return nil
+}
+
+func (m *ADCSnd) UnmarshalNMDC(dec *TextDecoder, data []byte) error {
+	i := bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCSnd: missing separator after field 1")
+	}
+	contentType, data := data[:i], data[i+1:]
+	if err := m.ContentType.UnmarshalNMDC(dec, contentType); err != nil {
+		return err
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCSnd: missing separator after field 2")
+	}
+	identifier, data := data[:i], data[i+1:]
+	if err := m.Identifier.UnmarshalNMDC(dec, identifier); err != nil {
+		return errors.New("ADCSnd: unable to parse field 2")
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("ADCSnd: missing separator after field 3")
+	}
+	start, data := data[:i], data[i+1:]
+	var err error
+	m.Start, err = parseUin64Trim(start)
+	if err != nil {
+		return errors.New("ADCSnd: unable to parse field 3")
+	}
+
+	i = bytes.IndexByte(data, ' ')
+	var length []byte
+	if i < 0 {
+		length, data = data[:], nil
+	} else {
+		length, data = data[:i], data[i+1:]
+	}
+	m.Length, err = parseUin64Trim(length)
+	if err != nil {
+		return errors.New("ADCSnd: unable to parse field 4")
+	}
+
+	if len(data) > 0 {
+		if !bytes.Equal(data, []byte("ZL1")) {
+			return errors.New("ADCSnd: invalid field 5")
+		}
+		m.Compressed = true
+	}
+
+	return nil
+}
+
+type Direction struct {
+	Upload bool
+	Number uint
+}
+
+func (*Direction) Type() string {
+	return "Direction"
+}
+
+func (m *Direction) MarshalNMDC(_ *TextEncoder, buf *bytes.Buffer) error {
+	if m.Upload {
+		buf.Write([]byte("Upload"))
+	} else {
+		buf.Write([]byte("Download"))
+	}
+	buf.WriteByte(' ')
+	buf.WriteString(strconv.Itoa(int(m.Number)))
+	return nil
+}
+
+func (m *Direction) UnmarshalNMDC(_ *TextDecoder, data []byte) error {
+	i := bytes.IndexByte(data, ' ')
+	if i < 0 {
+		return errors.New("Direction: missing field 1")
+	}
+	direction, data := data[:i], data[i+1:]
+	if bytes.Equal(direction, []byte("Download")) {
+		m.Upload = false
+	} else if bytes.Equal(direction, []byte("Upload")) {
+		m.Upload = true
+	} else {
+		return errors.New("Direction: invalid direction field")
+	}
+
+	number, err := parseUin64Trim(data)
+	if err != nil {
+		return errors.New("Direction: unable to parse field 2")
+	}
+	if number < 1 || number > 32767 {
+		return errors.New("Direction: number outside range")
+	}
+	m.Number = uint(number)
+
+	return nil
+}
+
+type MaxedOut struct {
+	NoArgs
+}
+
+func (*MaxedOut) Type() string {
+	return "MaxedOut"
+}
---
+++ nmdc/files_test.go
@@ -0,0 +1,101 @@
+package nmdc
+
+import (
+	"testing"
+)
+
+var filesCases = []casesMessageEntry{
+	{
+		typ:  "ADCGET",
+		data: `file files.xml.bz2 0 -1`,
+		msg: &ADCGet{
+			ContentType: "file",
+			Identifier:  "files.xml.bz2",
+			Start:       0,
+			Length:      -1,
+		},
+	},
+	{
+		typ:  "ADCGET",
+		data: `file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 123523 65`,
+		msg: &ADCGet{
+			ContentType: "file",
+			Identifier:  "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:       123523,
+			Length:      65,
+		},
+	},
+	{
+		typ:  "ADCGET",
+		data: `tthl TTH/PT2BL57H4JJ5LHXBDA6CJ5KEOO5XEKNIFYINE7I 6342 7323`,
+		msg: &ADCGet{
+			ContentType: "tthl",
+			Identifier:  "TTH/PT2BL57H4JJ5LHXBDA6CJ5KEOO5XEKNIFYINE7I",
+			Start:       6342,
+			Length:      7323,
+		},
+	},
+	{
+		typ:  "ADCGET",
+		data: `file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 123523 65 ZL1`,
+		msg: &ADCGet{
+			ContentType: "file",
+			Identifier:  "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:       123523,
+			Length:      65,
+			Compressed:  true,
+		},
+	},
+	{
+		typ:  "ADCSND",
+		data: `file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 123523 65`,
+		msg: &ADCSnd{
+			ContentType: "file",
+			Identifier:  "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:       123523,
+			Length:      65,
+		},
+	},
+	{
+		typ:  "ADCSND",
+		data: `tthl TTH/PT2BL57H4JJ5LHXBDA6CJ5KEOO5XEKNIFYINE7I 6342 7323`,
+		msg: &ADCSnd{
+			ContentType: "tthl",
+			Identifier:  "TTH/PT2BL57H4JJ5LHXBDA6CJ5KEOO5XEKNIFYINE7I",
+			Start:       6342,
+			Length:      7323,
+		},
+	},
+	{
+		typ:  "Direction",
+		data: `Download 12343`,
+		msg: &Direction{
+			Upload: false,
+			Number: 12343,
+		},
+	},
+	{
+		typ:  "Direction",
+		data: `Upload 6533`,
+		msg: &Direction{
+			Upload: true,
+			Number: 6533,
+		},
+	},
+}
+
+func TestFilesUnmarshal(t *testing.T) {
+	doMessageTestUnmarshal(t, filesCases)
+}
+
+func TestFilesMarshal(t *testing.T) {
+	doMessageTestMarshal(t, filesCases)
+}
+
+func BenchmarkFilesUnmarshal(b *testing.B) {
+	doMessageBenchmarkUnmarshal(b, filesCases)
+}
+
+func BenchmarkFilesMarshal(b *testing.B) {
+	doMessageBenchmarkMarshal(b, filesCases)
+}
