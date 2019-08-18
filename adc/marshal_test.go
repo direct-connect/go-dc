38c94bf540d073a67176017feccf2810a08525d6
gswly <46489434+gswly@users.noreply.github.com>
2019-08-18 20:00:11 +0200

    adc: support file compression

--- adc/marshal_test.go
+++ adc/marshal_test.go
@@ -127,6 +127,39 @@ var casesDecode = []struct {
 			Category: adc.CategoryHub | adc.CategoryUser,
 		},
 	},
+	{
+		"get file",
+		`file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352`,
+		&adc.GetRequest{
+			Type:       "file",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: false,
+		},
+	},
+	{
+		"get file compressed",
+		`file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352 ZL1`,
+		&adc.GetRequest{
+			Type:       "file",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: true,
+		},
+	},
+	{
+		"get tthl",
+		`tthl TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352`,
+		&adc.GetRequest{
+			Type:       "tthl",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: false,
+		},
+	},
 }
 
 func sidp(s string) *types.SID {
@@ -222,6 +255,36 @@ var casesEncode = []struct {
 			Category: adc.CategoryHub | adc.CategoryUser,
 		},
 		`ADCH++/Hub\smanagement/Reload\sscripts TTHMSG\s+reload\n CT3`,
+	},
+	{
+		&adc.GetRequest{
+			Type:       "file",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: false,
+		},
+		`file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352`,
+	},
+	{
+		&adc.GetRequest{
+			Type:       "file",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: true,
+		},
+		`file TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352 ZL1`,
+	},
+	{
+		&adc.GetRequest{
+			Type:       "tthl",
+			Path:       "TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI",
+			Start:      124,
+			Bytes:      12352,
+			Compressed: false,
+		},
+		`tthl TTH/BR4BVJBMHDFVCFI4WBPSL63W5TWXWVBSC574BLI 124 12352`,
 	},
 }
 
