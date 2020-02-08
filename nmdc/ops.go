83d67d4824b54fb6ff4d1127cc428ad9bbc2dc00
Denys Smirnov <denis.smirnov.91@gmail.com>
2020-02-08 18:04:44 +0200

    nmdc: add a few more missing messages

--- nmdc/ops.go
+++ nmdc/ops.go
@@ -6,6 +6,10 @@ import (
 
 func init() {
 	RegisterMessage(&ForceMove{})
+	RegisterMessage(&Kick{})
+	RegisterMessage(&Close{})
+	RegisterMessage(&GetTopic{})
+	RegisterMessage(&SetTopic{})
 }
 
 type ForceMove struct {
@@ -24,4 +28,44 @@ func (m *ForceMove) MarshalNMDC(enc *TextEncoder, buf *bytes.Buffer) error {
 func (m *ForceMove) UnmarshalNMDC(dec *TextDecoder, data []byte) error {
 	m.Address = string(data)
 	return nil
+}
+
+var _ Message = (*Kick)(nil)
+
+type Kick struct {
+	Name
+}
+
+func (*Kick) Type() string {
+	return "Kick"
+}
+
+var _ Message = (*Close)(nil)
+
+type Close struct {
+	Name
+}
+
+func (*Close) Type() string {
+	return "Close"
+}
+
+var _ Message = (*GetTopic)(nil)
+
+type GetTopic struct {
+	NoArgs
+}
+
+func (*GetTopic) Type() string {
+	return "GetTopic"
+}
+
+var _ Message = (*SetTopic)(nil)
+
+type SetTopic struct {
+	String
+}
+
+func (*SetTopic) Type() string {
+	return "SetTopic"
 }
